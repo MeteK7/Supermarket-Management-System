9302c1c531d86b8d3296d69838253424ff8948cd
Mete <metekaba96@gmail.com>
2022-02-12 23:57:05 +0300

    Some developments.

---
+++ Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using CoreBusiness;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+        public CategoryInMemoryRepository()
+        {
+            categories = new List<Category>()
+            {
+                new Category(){CategoryId=1,Name="Beverage",Description="Beverage" },
+                new Category(){CategoryId=2,Name="Bakery",Description="Bakery" },
+                new Category(){CategoryId=3,Name="Meat",Description="Meat" }
+            };
+        }
+        public IEnumerable<Category> GetCategories()
+        {
+            return categories;
+        }
+    }
+}
